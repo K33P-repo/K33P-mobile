d5b0fd717ead516fbf728c7f31caad4ceb5e9e00
Hardeezah <mohammedhardeeza@gmail.com>
2025-05-16 14:59:40 +0100

    changes

--- app/(auth)/sign-up/index.tsx
+++ app/(auth)/sign-up/index.tsx
@@ -1,19 +1,10 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import TextInput from '../../../components/TextInput';
+import React from 'react'
+import { Text, View } from 'react-native'
 
-export default function Index() {  // Component names should be PascalCase
+export default function index() {
   return (
     <View>
       <Text>index</Text>
-      <TextInput 
-        title="Enter Phone Number" 
-        placeholder="+234-801-2345-678" 
-        initialValue="" 
-        minLength={10}  // No extra parentheses needed
-        onValidChange={(isValid) => console.log(isValid)} 
-        autoFocus={true}  // Explicit boolean for consistency
-      />
     </View>
-  );
+  )
 }
